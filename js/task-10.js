4e484a30abef1af57fba45112d76004537d55834
Anna Ojdana <anna.ojdana@gmail.com>
2022-05-15 19:12:55 +0200

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
+const inputNumber = document.querySelector("input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", () => {
+  const boxAll = document.querySelectorAll('#boxes div');
+  boxAll.forEach(box => box.remove());
+});
+
+
+
+function createBoxes(amount) {
+  amount = inputNumber.value;
+  const box = [];
+  for (let i = 0; i < amount; i++) {
+    let size = 30 + i * 10;
+    let color = getRandomHexColor();
+  
+    box.push(`<div style="height:${size}px ; width:${size}px ; background-color: ${color};"></div>`);
+  }
+  
+  const markup = box.join("");
+  return boxes.insertAdjacentHTML("afterbegin", markup);
+
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
