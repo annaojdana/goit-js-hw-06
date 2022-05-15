ca63c29cfd4896c35c3e5ae3acd442e50955f5ea
Anna Ojdana <anna.ojdana@gmail.com>
2022-05-15 09:49:55 +0200

    task02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,4 +7,21 @@ const ingredients = [
   'Condiments',
 ];
 
-const ingredientsUl = 
+const ingredientsUl = document.querySelector('#ingredients')
+
+const li1 = document.createElement('li');
+li1.textContent = ingredients[0];
+
+const li2 = document.createElement('li');
+li2.textContent = ingredients[1];
+
+const li3 = document.createElement('li');
+li3.textContent = ingredients[2];
+
+const li4 = document.createElement('li');
+li4.textContent = ingredients[3];
+
+const li5 = document.createElement('li');
+li5.textContent = ingredients[4];
+
+ingredientsUl.append(li1, li2, li3, li4, li5);
