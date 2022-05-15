9c43b81b69d15aa31b0dc8ed75474ca15977b961
Anna Ojdana <anna.ojdana@gmail.com>
2022-05-15 17:49:02 +0200

    task09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const body = document.querySelector('body');
+const btnChange = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+
+btnChange.addEventListener("click", () => {
+  let randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+} )
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
