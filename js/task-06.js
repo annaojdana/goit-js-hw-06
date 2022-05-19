7099fcc4037db7cbd47180805a13d05024e142f0
Anna Ojdana <anna.ojdana@gmail.com>
2022-05-19 08:42:53 +0200

    bug-fix  task06

--- js/task-06.js
+++ js/task-06.js
@@ -2,6 +2,6 @@ const textInput = document.querySelector("#validation-input");
 const signLength = textInput.dataset.length;
 
 textInput.addEventListener("blur", () => {
-    (textInput.value.length < signLength && textInput.value.length > signLength ) ? textInput.className = "invalid" : 
+    (textInput.value.length = signLength ) ? textInput.className = "invalid" : 
     textInput.className = "valid" ;
 });
